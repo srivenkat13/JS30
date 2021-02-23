800367e648851e6c1bfb1d08b0ac1d8d986fb8e6
venkat <srivenkatsairaj13@gmail.com>
2021-02-23 12:46:20 +0530

    Updated JS

--- 01 - JavaScript Drum kit/script.js
+++ 01 - JavaScript Drum kit/script.js
@@ -1,7 +1,38 @@
-window.addEventListener('keydown', function (e) {
-    const AUDIO = document.querySelector(`audio[data-key="${e.keyCode}"]`) 
+function playSound(e) {
     //returns the audio element
-    const KEY =document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     // returns the div element which matches to keydown
-    console.log(KEY);
-})
+    const KEY = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+     //this rewinds the tone each time
+    audio.currentTime = 0;
+    audio.play();
+    // when a non-existent key is hit, it will return 
+    if (!audio) return;
+    KEY.classList.add('playing')
+}
+function removeTransition(e) {
+    //skip it if its not a transform property
+    if (e.propertyName !== 'transform') return; 
+    this.classList.remove('playing')
+}
+// click functionality 
+const items = document.querySelectorAll('.key');
+function clickTrigger() {
+    let key = (this.dataset.key);
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    this.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+}
+
+function unclickTrigger() {
+    this.classList.remove('playing');
+}
+items.forEach(item => item.addEventListener('mousedown', clickTrigger));
+items.forEach(item => item.addEventListener('mouseup', unclickTrigger));
+
+
+window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
