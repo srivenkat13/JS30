712666b1b5b325edcc96a65d2383c3f27a3a4c20
venkat <srivenkatsairaj13@gmail.com>
2021-02-26 22:51:29 +0530

    map method

--- 04-Array-1/script.js
+++ 04-Array-1/script.js
@@ -33,7 +33,8 @@ const fifteen = inventors.filter(inventor =>
 console.table(fifteen)
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
-
+const fullNames = inventors.map(inventor => (`${inventor.first} ${inventor.last}`));
+console.table(fullNames);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
