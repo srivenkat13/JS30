dc9e46613f1f03b8b87392853af1ebfb9ff17cfd
venkat <srivenkatsairaj13@gmail.com>
2021-09-27 23:01:37 +0530

    8-HTML5-canvas:Start

---
+++ 08-HTML5-canvas/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#plane");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.strokeStyle = "#BADA55";
