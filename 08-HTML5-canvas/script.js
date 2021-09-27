b37f583c93a3602e479b7fcdef318b0a399bc510
venkat <srivenkatsairaj13@gmail.com>
2021-09-27 23:22:37 +0530

    added basic canvas to draw

--- 08-HTML5-canvas/script.js
+++ 08-HTML5-canvas/script.js
@@ -4,4 +4,33 @@ const context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.strokeStyle = "#BADA55";
+context.strokeStyle = "#ca9bf7";
+context.lineJoin = "round";
+context.lineCap = "round";
+context.lineWidth = 5;
+
+//variables to for drawing the line
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+
+function draw(e) {
+    console.log(e);
+    
+    context.beginPath();
+
+    context.moveTo( lastX, lastY);
+    
+    context.lineTo(e.offsetX , e.offsetY);
+
+    context.stroke();
+
+}
+
+canvas.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => isDrawing = false );
+canvas.addEventListener("mouseout", () => isDrawing = false );
